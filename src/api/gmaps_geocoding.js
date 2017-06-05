7615e54f619e0de6e712c098abe3a9a0a83573cb
Ridho Pratama <99ridho@gmail.com>
2017-06-05 16:43:24 +0700

    firebase trayek edit1

---
+++ src/api/gmaps_geocoding.js
@@ -0,0 +1,8 @@
+import { GMAPS_GEOCODING_API_KEY } from '../utils/gmaps_config';
+
+export const geocodeAddress = (address) => {
+  const requestUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + GMAPS_GEOCODING_API_KEY;
+
+  return fetch(requestUrl)
+          .then(response => response.json());
+}
---
+++ src/utils/gmaps_config.js
@@ -0,0 +1,1 @@
+export const GMAPS_GEOCODING_API_KEY = "AIzaSyCZ8JmauGtW_3CnmuXiiNodKdZ1ACajxC4";
